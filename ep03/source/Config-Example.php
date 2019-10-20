33e2bfbab827076a40fc415b5b86d978c2e61be9
Vinicius Mattos <mvinicius@funcionalcorp.com.br>
2019-10-20 15:01:43 -0300

    20102019 - Composer lock.

---
+++ ep03/source/Config-Example.php
@@ -0,0 +1,10 @@
+<?php
+
+define("MAIL", [
+    "host"       => "",
+    "port"       => "",
+    "user"       => "",
+    "passwd"     => "",
+    "from_name"  => "Vinicius Mattos",
+    "from_email" => "vinimattos.rj@gmail.com"
+]);
