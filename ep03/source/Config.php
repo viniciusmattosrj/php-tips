9642a4c7038c02c68cbbe3c25c1d28a6c50d7b8d
Vinicius Mattos <mvinicius@funcionalcorp.com.br>
2019-10-20 23:06:08 -0300

    20102019 - Update.

--- ep03/source/Config.php
+++ ep03/source/Config.php
@@ -1,2 +1,10 @@
 <?php
 
+define("MAIL", [
+    "host"       => "smtp.mailtrap.io",
+    "port"       => "2525",
+    "user"       => "d92176089c6000",
+    "passwd"     => "d8acf5f8cb6868",
+    "from_name"  => "Vinicius Mattos",
+    "from_email" => "vinimattos.rj@gmail.com"
+]);
