c95b893eec0ca5d20c93a9603e034b240e66e660
Vinicius Mattos <mvinicius@funcionalcorp.com.br>
2019-10-21 13:47:06 -0300

    21102019 - Gerando blog com paginação resultados.

---
+++ ep05/generate.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__ ."/vendor/autoload.php";
+
+use Faker\Provider\Image;
+use Faker\Provider\Lorem;
+use Source\Models\Post;
+
+for ($i = 0; $i < 1; $i++) {
+    $post = new Post();
+    $post->title = Lorem::text(80);
+    $post->cover = Image::image("images", 300, 150);
+    $post->description = Lorem::paragraphs(2,  true);
+
+    var_dump($post);
+}
